157908b795fbc36e5cd49fc1553357c64c7fbe9b
Alex <alexikbiznes@gmail.com>
2024-02-08 19:25:51 +0100

    Stronka walentynki

---
+++ script.js
@@ -0,0 +1,9 @@
+document.getElementById("tak").addEventListener("click", function () {
+  document.getElementById("takimg").style.display = "block";
+  document.querySelector(".form").style.display = "none";
+});
+
+document.getElementById("nie").addEventListener("click", function () {
+  document.getElementById("nieimg").style.display = "block";
+  document.querySelector(".form").style.display = "none";
+});
